78d9033bf38445611d25beda81a2c68ba3231042
kuchimanchi <kuchimanchi.22@mountblue.tech>
2022-11-27 01:18:19 +0530

    modified again

--- q6.py
+++ q6.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 
 def calculate(matches):
     '''for calculating'''
+    seasons = set()
+    teams = set()
     match_data = []
     with open(matches, 'r', encoding='utf-8') as file:
         matches = csv.DictReader(file)
@@ -21,138 +23,69 @@ def calculate(matches):
     seasons = list((seasons))
     teams = list((teams))
     teams.sort()
+    played = [0]*len(teams)
     seasons.sort()
-    seasons_and_teams = {}
+    # print(seasons,teams)
+    teams_played = dict(zip(teams, played))
+    print(teams_played)
+    year_team_played = {}
+    for year in seasons:
+        year_team_played[year] = teams_played.copy()
+    print(year_team_played)
 
     for match in match_data:
+        if match['winner'] == match['team1']:
+            year_team_played[match['season']][match['winner']] += 1
+        elif match['winner'] == match['team2']:
+            year_team_played[match['season']][match['winner']] += 1
 
-        year = match['season']
-        team1 = match['team1']
-        team2 = match['team2']
-
-        if year in seasons:
-
-            if year not in seasons_and_teams:
-                seasons_and_teams[year] = {}
-                winner = match['winner']
-                if (team1 not in seasons_and_teams[year] and winner == team1):
-                    seasons_and_teams[year][team1] = 1
-                elif team1 in seasons_and_teams[year] and winner == team1:
-                    seasons_and_teams[year][team1] += 1
-                else:
-                    pass
-                if team2 not in seasons_and_teams[year] and winner == team2:
-                    seasons_and_teams[year][team2] = 1
-                elif team1 in seasons_and_teams[year] and winner == team2:
-                    seasons_and_teams[year][team2] += 1
-            else:
-                if team1 not in seasons_and_teams[year] and winner == team1:
-                    seasons_and_teams[year][team1] = 1
-                elif team1 in seasons_and_teams[year] and winner == team1:
-                    seasons_and_teams[year][team1] += 1
-                else:
-                    pass
-                if team2 not in seasons_and_teams[year] and winner == team2:
-                    seasons_and_teams[year][team2] = 1
-                elif team1 in seasons_and_teams[year] and winner == team2:
-                    seasons_and_teams[year][team2] += 1
-                else:
-                    pass
-
+    year_wise_games_won = []
     for year in seasons:
-        for team in teams:
-            if team not in seasons_and_teams[year]:
-                seasons_and_teams[year][team] = 0
-    return seasons_and_teams
+        year_wise_games_won.append(list(year_team_played[year].values()))
+
+    print(year_wise_games_won)
+    return [seasons, year_wise_games_won]
 
 
 def tranform(seasons_and_teams_matches):
     '''for transforming'''
-    final = list(seasons_and_teams_matches.values())
-    match_count = []
-    for year in range(10):
-        count = list(final[year].values())
-        match_count.append(count)
+    transformed_data = [[row[i] for row in seasons_and_teams_matches]
+                        for i in range(len(seasons_and_teams_matches[0]))]
 
-    print(match_count)
-    seasons_and_teams_matches_final = list(map(list, zip(*match_count)))
-
-    return seasons_and_teams_matches_final
+    return transformed_data
 
 
 def plot(teams, seasons):
+    '''for plotting'''
+    _, aux = plt.subplots()
 
-    _, ax = plt.subplots()
-    team2start = [teams[0][i]+teams[1][i] for i in range(len(teams[0]))]
-    team3start = [teams[0][i]+teams[1][i]+teams[2][i]
-                  for i in range(len(teams[0]))]
-    team4start = [teams[0][i]+teams[1][i]+teams[2][i]+teams[3][i]
-                  for i in range(len(teams[0]))]
-    team5start = [teams[0][i]+teams[1][i]+teams[2][i] +
-                  teams[3][i]+teams[4][i] for i in range(len(teams[0]))]
-    team6start = [teams[0][i]+teams[1][i]+teams[2][i]+teams[3]
-                  [i]+teams[4][i]+teams[5][i] for i in range(len(teams[0]))]
-    team7start = [teams[0][i]+teams[1][i]+teams[2][i]+teams[3][i] +
-                  teams[4][i]+teams[5][i]+teams[6][i]
-                  for i in range(len(teams[0]))]
-    team8start = [teams[0][i]+teams[1][i]+teams[2][i]+teams[3][i]+teams[4]
-                  [i]+teams[5][i]+teams[6][i]+teams[7][i]
-                  for i in range(len(teams[0]))]
-    team9start = [teams[0][i]+teams[1][i]+teams[2][i]+teams[3][i]+teams[4][i] +
-                  teams[5][i]+teams[6][i]+teams[7][i]+teams[8][i]
-                  for i in range(len(teams[0]))]
-    team10start = [teams[0][i]+teams[1][i]+teams[2][i] +
-                   teams[3][i]+teams[4][i] + teams[5][i]+teams[6][i] +
-                   teams[7][i]+teams[8][i]+teams[9][i]
-                   for i in range(len(teams[0]))]
-    team11start = [teams[0][i]+teams[1][i]+teams[2][i] +
-                   teams[3][i]+teams[4][i] +
-                   teams[5][i]+teams[6][i]+teams[7][i]
-                   + teams[8][i]+teams[9][i] +
-                   teams[10][i] for i in range(len(teams[0]))]
-    team12start = [teams[0][i]+teams[1][i]+teams[2][i]+teams[3][i]+teams[4][i]
-                   + teams[5][i]+teams[6][i]+teams[7][i] +
-                   teams[8][i]+teams[9][i]+teams[10][i] +
-                   teams[11][i] for i in range(len(teams[0]))]
-    team13start = [teams[0][i]+teams[1][i]+teams[2][i]+teams[3][i]+teams[4][i]
-                   + teams[5][i]+teams[6][i]+teams[7][i]+teams[8][i]
-                   + teams[9][i]+teams[10][i]+teams[11][i]+teams[12][i]
-                   for i in range(len(teams[0]))]
+    teamstart = [teams[0], teams[1]]
 
-    ax.bar(seasons, teams[0], width=0.2, color='yellow', label='csk')
-    ax.bar(seasons, teams[1], bottom=teams[0],
-           width=0.2, color='black', label='dc')
-    ax.bar(seasons, teams[2], bottom=team2start,
-           width=0.2, color='pink', label='dd')
-    ax.bar(seasons, teams[3], bottom=team3start,
-           width=0.2, color='gold', label='gl')
-    ax.bar(seasons, teams[4], bottom=team4start,
-           width=0.2, color='tomato', label='kxip')
-    ax.bar(seasons, teams[5], bottom=team5start,
-           width=0.2, color='purple', label='ktk')
-    ax.bar(seasons, teams[6], bottom=team6start,
-           width=0.2, color='darkcyan', label='kkr')
-    ax.bar(seasons, teams[7], bottom=team7start,
-           width=0.2, color='cyan', label='mi')
-    ax.bar(seasons, teams[8], bottom=team8start,
-           width=0.2, color='red', label='pwi')
-    ax.bar(seasons, teams[9], bottom=team9start,
-           width=0.2, color='brown', label='rr')
-    ax.bar(seasons, teams[10], bottom=team10start,
-           width=0.2, color='bisque', label='rpsgs')
-    ax.bar(seasons, teams[11], bottom=team11start,
-           width=0.2, color='slategrey', label='rpsg')
-    ax.bar(seasons, teams[12], bottom=team12start,
-           width=0.2, color='lime', label='rcb')
-    ax.bar(seasons, teams[13], bottom=team13start,
-           width=0.2, color='forestgreen', label='srh')
+    for k in range(2, 14):
+
+        teams_to_consider = [teams[j] for j in range(0, k)]
+        valuesof_last_appended_team_ongraph = [
+            sum(group) for group in zip(*teams_to_consider)]
+        teamstart.append(valuesof_last_appended_team_ongraph)
+
+    print(teamstart)
+
+    colors = ['yellow', 'black', 'pink', 'gold', 'tomato', 'purple', 'red',
+              'cyan', 'brown', 'bisque', 'lime', 'slategrey',
+              'forestgreen', 'green']
+    labels = ['csk', 'dc', 'dd', 'gl', 'kxip', 'ktk', 'kkr',
+              'mi', 'pwi', 'rr', 'rpsgs', 'rpsg', 'rcb', 'srh']
+    aux.bar(seasons, teams[0], width=0.2, color=colors[0], label=labels[0])
+    aux.bar(seasons, teams[1], bottom=teams[0],
+            width=0.2, color=colors[1], label=labels[1])
+    for values in range(2, 13):
+        aux.bar(seasons, teams[values], bottom=teamstart[values],
+                width=0.2, color=colors[values], label=labels[values])
     plt.legend(title='teams', loc='upper right')
-
     plt.show()
 
 
-seasons_and_teamswins = calculate("matches.csv")
-seasons_and_teams_totalwins = tranform(seasons_and_teamswins)
-print(seasons_and_teamswins.keys())
-print(len(seasons_and_teams_totalwins))
-plot(seasons_and_teams_totalwins, list(seasons_and_teamswins.keys()))
+seasons_and_teams_matches_played = calculate("matches.csv")
+seasons_and_teams_total = tranform(seasons_and_teams_matches_played[1])
+print(seasons_and_teams_total)
+plot(seasons_and_teams_total, seasons_and_teams_matches_played[0])
